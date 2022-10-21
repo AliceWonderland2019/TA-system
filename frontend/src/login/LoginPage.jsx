52f3f10e265466f4cff59b27f540fe61c3683480
Rofo <88415585+rudycito23@users.noreply.github.com>
2022-10-21 15:22:40 -0500

    Updatd styling to Login page and Register page.

--- frontend/src/login/LoginPage.jsx
+++ frontend/src/login/LoginPage.jsx
@@ -1,47 +1,60 @@
-import './LoginPage.css';
-import { useState } from 'react';
-import { Link,useNavigate} from 'react-router-dom';
+import "./LoginPage.css";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { checkAccount } from "../api/UserApi";
-import { StudentHome } from '../home/StudentHome';
+import { StudentHome } from "../home/StudentHome";
 
+export const LoginPage = () => {
+  // //localStorage.clear();
+  const [userName, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-export const LoginPage =()=>{
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
 
-    // //localStorage.clear();
-    const [userName, setName]=useState('');
-    const [password, setPassword]=useState('');
-    const navigate=useNavigate();
+  const handleChangePW = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleChangeName=(e)=>{
-        setName(e.target.value);
-    }
+  const handleSubmitClick = () => {
+    localStorage.clear();
+    checkAccount(userName, password);
+    // navigate('./studentHome');
+  };
 
-    const handleChangePW=(e)=>{
-        setPassword(e.target.value);
-    }
-    
-    const handleSubmitClick=()=>{
-        localStorage.clear();
-        checkAccount(userName,password);
-        // navigate('./studentHome');
-    }
-
-    return(
-    <div className='Login-component'>
-        
-    <div className="whiteBox">
-        <img src={"logo1(6).png"} className="logo"/>
+  return (
+    <div className="Login-component">
+      <div className="whiteBox">
+        <img src={"logo1(6).png"} className="logo" />
         <h1>Log In</h1>
-        <input type="text" class="form-control" placeholder='User Name' value={userName} onChange={handleChangeName}/>
-        <br/>
-        <br/>
-        <input type="password" class="form-control" placeholder='Password' value={password} onChange={handleChangePW}/>
-        <br/>
-        <br/>
-    <button variant="outlined" onClick={handleSubmitClick}>Submit</button>
-    <br/>
-        <h6><Link to='/register' className="register">Don't have an account? Click here to make one!</Link></h6>
-   </div>
-   </div>
-    );
+        <input
+          type="text"
+          class="form-control"
+          placeholder="User Name"
+          value={userName}
+          onChange={handleChangeName}
+        />
+        <br />
+        <input
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          value={password}
+          onChange={handleChangePW}
+        />
+        <br />
+        <button class="login-button" type="submit" onClick={handleSubmitClick}>
+          Submit
+        </button>
+        <br />
+        <h6>
+          <Link to="/register" className="register">
+            Don't have an account? Click here to register!
+          </Link>
+        </h6>
+      </div>
+    </div>
+  );
 };
--- frontend/src/register/Register.jsx
+++ frontend/src/register/Register.jsx
@@ -1,55 +1,53 @@
-import './Register.css';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import "./Register.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import { createAccountStudent } from "../api/UserApi";
 
 export const Register = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [ID, setID] = useState(null);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
-    const [userName, setUserName]=useState('');
-    const [password, setPassword] = useState('');
-    const [ID, setID] = useState(null);
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-   
-    // States for checking the errors
-    const [submitted, setSubmitted] = useState(false);
-    const [numError, setNumError] = useState(false);
-    //const [error, setError] = useState(false);
-  
-    // Handling the username change
-    const handleUsername = (e) => {
-      setUserName(e.target.value);
-      setSubmitted(false);
-   };
-    //Handling the form submission
-    const handleID = (e) => {
-      if(!isNaN(e.target.value)){
+  // States for checking the errors
+  const [submitted, setSubmitted] = useState(false);
+  const [numError, setNumError] = useState(false);
+  //const [error, setError] = useState(false);
+
+  // Handling the username change
+  const handleUsername = (e) => {
+    setUserName(e.target.value);
+    setSubmitted(false);
+  };
+  //Handling the form submission
+  const handleID = (e) => {
+    if (!isNaN(e.target.value)) {
       setID(e.target.value);
       setSubmitted(false);
       setNumError(false);
-      }
-      else{
-       e.target.value=null; //get rid of initial
-       setNumError(true);
-      }
+    } else {
+      e.target.value = null; //get rid of initial
+      setNumError(true);
+    }
   };
 
-    // Handling the password change
-    const handlePassword = (e) => {
-      setPassword(e.target.value);
-      setSubmitted(false);
-    };   
+  // Handling the password change
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    setSubmitted(false);
+  };
 
   // Handling the name change
-    const handleFirstName = (e) => {
-      setFirstName(e.target.value);
-      setSubmitted(false);
-    };
-    // Handling the name change
-    const handleLastName = (e) => {
-      setLastName(e.target.value);
-      setSubmitted(false);
-    };
+  const handleFirstName = (e) => {
+    setFirstName(e.target.value);
+    setSubmitted(false);
+  };
+  // Handling the name change
+  const handleLastName = (e) => {
+    setLastName(e.target.value);
+    setSubmitted(false);
+  };
 
   // Showing error message if error is true
   const errorNum = () => {
@@ -57,8 +55,9 @@ export const Register = () => {
       <div
         className="numError"
         style={{
-          display: numError ? '' : 'none',
-        }}>
+          display: numError ? "" : "none",
+        }}
+      >
         <p>*ID can only be numbers</p>
       </div>
     );
@@ -67,59 +66,64 @@ export const Register = () => {
   //Handling the form submission
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (userName === '' || password === ''||ID ==='') {
+    if (userName === "" || password === "" || ID === "") {
       window.alert("Please fill out User Name & Password & SMU ID.");
     } else {
       setSubmitted(true);
       //setError(true);
-      createAccountStudent(userName, password, firstName, lastName, ID);  
+      createAccountStudent(userName, password, firstName, lastName, ID);
     }
   };
 
- 
-    return(<div className='Register-component'>
-    <div className="whiteBox">
-    <img src={"logo1(6).png"} className="logo"/>
-    <h1>Register</h1> 
+  return (
+    <div className="Register-component">
+      <div className="whiteBox">
+        <img src={"logo1(6).png"} className="logo" />
+        <h2>Register</h2>
 
-    <div className="form">
-    <label className="form-control">*User Name:</label>
-    <br/>
-    <input type="text" value={userName} onChange={handleUsername}/>
-    <br/>
-    <br/>
+        <div className="form">
+          <label className="form-control">*User Name:</label>
+          <br />
+          <input type="text" value={userName} onChange={handleUsername} />
+          <br />
+          <br />
 
-    <label className="form-control">*Password:</label>
-    <br/>
-    <input type="password" value={password} onChange={handlePassword}/>
-    <br/>
-    <br/>
+          <label className="form-control">*Password:</label>
+          <br />
+          <input type="password" value={password} onChange={handlePassword} />
+          <br />
+          <br />
 
-    <label className="form-control">*SMU ID:</label>
-    <br/>
-    <div className="message">{errorNum()}</div>
-    <input type="text" value={ID} onChange={handleID}/>
-    <br/>
-    <br/>
+          <label className="form-control">*SMU ID:</label>
+          <br />
+          <div className="message">{errorNum()}</div>
+          <input type="text" value={ID} onChange={handleID} />
+          <br />
+          <br />
 
-    <label className="form-control">FirstName:</label>
-    <br/>
-    <input type="text" value={firstName} onChange={handleFirstName}/>
-    <br/>
-    <br/>
+          <label className="form-control">FirstName:</label>
+          <br />
+          <input type="text" value={firstName} onChange={handleFirstName} />
+          <br />
+          <br />
 
-    <label className="form-control">Last Name: </label>
-    <br/>
-    <input type="text" value={lastName} onChange={handleLastName}/>
-    <br/>
-    <br/>
-  
-    <div className="blocks">
-   <button variant="outlined" onClick={handleSubmit}>Create account</button>
-   <button variant="outlined"><Link to='/' className="LoginPage">Back</Link></button>
-   </div>
-   
-   </div>
-   </div>
-    </div>);
-}
+          <label className="form-control">Last Name: </label>
+          <br />
+          <input type="text" value={lastName} onChange={handleLastName} />
+          <br />
+          <br />
+          <div className="register-buttons">
+            <button variant="outlined" onClick={handleSubmit}>
+              Create account
+            </button>
+            <button variant="outlined">
+              <Link to="/" className="LoginPage">
+                Back
+              </Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
