54509c5a333108ec7f86f25638604ab046dbed94
AliceWonderland2019 <alice100gm@gmail.com>
2022-11-18 17:09:36 -0600

    update job list

--- frontend/src/home/StudentHome.jsx
+++ frontend/src/home/StudentHome.jsx
@@ -1,29 +1,33 @@
 import "./StudentHome.css";
 import { useState,useEffect } from 'react';
 import { getUserInfo } from "../api/UserApi";
-import { Link } from "react-router-dom";
 import {StudentNavBar} from "../Navbar/StudentNavBar";
 import { JobMarket } from "./JobMarket";
-
+import {getJobList} from "../api/JobApi";
 
 export const StudentHome = () => {
 
     const [user, setUser]=useState(undefined);
+    const [Jobs, setJobs ]=useState([]);
   
     useEffect(()=>{
         getUserInfo().then(x => setUser(x))
     },[ ]);
-  
+    
+    useEffect(()=>{
+        getJobList().then(y => setJobs(y));
+    },[ ]);
+
      if(!user){
         return<>Please log in first</>
     }
-  
+
     return(<div className="HomeContainer">
         <StudentNavBar/>
           <div className="MainContainer">
-              <h1>Hello!! {user[0].username}</h1>
+              <h1>Hello!! {user[0].username}</h1>      
+              <JobMarket Jobs={Jobs}/>
           </div>
-        <JobMarket/>
       </div>
   
       );
